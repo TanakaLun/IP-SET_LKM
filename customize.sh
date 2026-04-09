32b9c60d132a6b1ec62d3ef840b310a9d18149a3
TanakaLun <2709410138@qq.com>
2026-04-10 01:48:22 +0800

    fix: Improper permission group settings prevent IPSET binary from being called.

--- customize.sh
+++ customize.sh
@@ -5,11 +5,12 @@ if [ -z "$KSU" ]; then
     abort
 fi
 
-set_perm "$MODPATH/bin/ipset" 0 0 0777
+mv -f "$MODPATH/bin/ipset" "/data/adb/ksu/bin/"
+rm -rf "$MODPATH/bin/ipset"
 
 [ -d "$MODPATH/netfilter" ] && {
     rm -rf "/data/adb/netfilter"
     mv "$MODPATH/netfilter" "/data/adb/"
     chmod -R 755 "/data/adb/netfilter"
-    ui_print "- IP-SET Is Installed"
+    ui_print "- IP-SET has been installed"
 }
