4a698bcf181df37c352c0cd2fe963368d195c9cd
TanakaLun <2709410138@qq.com>
2026-04-10 18:16:11 +0800

    fix: incorrect handling of ipset binary permissions during module installation caused ispet to be inaccessible.

--- customize.sh
+++ customize.sh
@@ -20,6 +20,7 @@ else
 fi
 
 set_perm_recursive "$TARGET_DIR/bin" 0 0 0755 0755
+set_perm_recursive "$MODPATH/bin" 0 0 0755 0755
 
 BIN_DIR=$(echo "$PATH" | tr ':' '\n' | grep '/data/.*bin' | head -1)
 
